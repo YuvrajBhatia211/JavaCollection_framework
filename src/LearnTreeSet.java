76ccab38df52e323633b94424e6077de0218cf37
yuvraj <bhatiayuvraj98@gmail.com>
2026-03-22 16:34:15 +0530

    TreeSEt

---
+++ src/LearnTreeSet.java
@@ -0,0 +1,16 @@
+import java.util.TreeSet;
+
+public class LearnTreeSet {
+
+    public static void main(String[] args){
+        TreeSet<Integer> tr = new TreeSet<>();
+
+        //its same as hashset
+        //Same properities
+        //No duplication of elements
+
+        //Just one difference that it helps to create binary search tree
+        //thats why elements are always stored in sorted way no matter how they are being added
+
+    }
+}
