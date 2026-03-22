8edaac562c51960a6d63574341a2fa2fbfb0b19d
yuvraj <bhatiayuvraj98@gmail.com>
2026-03-22 16:35:26 +0530

    TreeSEt

--- src/LearnTreeSet.java
+++ src/LearnTreeSet.java
@@ -12,5 +12,9 @@ public class LearnTreeSet {
         //Just one difference that it helps to create binary search tree
         //thats why elements are always stored in sorted way no matter how they are being added
 
+
+        //one more thing
+        //Hash set - O(1)
+        //tree set - o(logn)
     }
 }
